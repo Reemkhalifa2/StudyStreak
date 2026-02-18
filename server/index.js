85e327ceac02c842b0db4d6e86da95005774717f
Reemkhalifa2 <aladawi12reem6@gmail.com>
2026-02-18 14:54:44 +0400

    re structure files

--- server/index.js
+++ server/index.js
@@ -12,11 +12,28 @@ app.use(cors());
 const connectString = "mongodb+srv://aladawi12reem6_db_user:reem123@studystreak.ru3bmvp.mongodb.net/Studystreak?appName=Studystreak";
 
 mongoose.connect(connectString)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.error("MongoDB connection error:", err));
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+app.post("/registerUser", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await UserModel.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new UserModel({ name, email, password: hashedPassword });
+    await user.save();
+
+    res.status(201).json({ user, msg: "User added." });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "An error occurred" });
+  }});
+
+app.listen(3001, () => {
+    console.log("Server running");
 });
-
-app.post("")
